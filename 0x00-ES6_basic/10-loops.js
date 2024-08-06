2576721a0e00629406bdc360bdff94423686519f
Kyalo460 <Kyalo460@gmail.com>
2024-08-06 11:00:57 +0300

    shortened code

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [...array];
-  let idx = 0;
-  for (const value of newArray) {
-    newArray[idx] = appendString + value;
-    idx += 1;
+  for (const [idx, value] of array.entries()) {
+    array[idx] = appendString + value;
   }
 
-  return newArray;
+  return array;
 }
