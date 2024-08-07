e1eb8e428084a54f50b338c191c95180170d7c37
Kyalo460 <Kyalo460@gmail.com>
2024-08-07 18:50:41 +0300

    fixed lint errors

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-
-  try{
-    let photo = await uploadPhoto();
-    let user = await createUser();
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
 
     const object = {
       photo,
@@ -12,11 +11,10 @@ export default async function asyncUploadUser() {
     };
 
     return object;
-
-  } catch(error) {
+  } catch (error) {
     return {
       photo: null,
       user: null,
-    }
+    };
   }
 }
