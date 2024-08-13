01256efbd135814b9f6405e9d903b198827ea26c
Kyalo460 <Kyalo460@gmail.com>
2024-08-13 06:52:44 +0300

    throwed error if index out of bounds

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,11 @@ export default function createInt8TypedArray(length, position, value) {
   const arrayBuffer = new ArrayBuffer(length);
   const view = new DataView(arrayBuffer);
 
-  view.setInt8(position, value);
+  try {
+    view.setInt8(position, value);
+  } catch (error) {
+    throw Error('Position outside range');
+  }
 
   return view;
 }
